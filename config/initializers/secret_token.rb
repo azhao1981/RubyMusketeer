76009c95cc9a30a6732f767aa9a364b028bb2958
= <=>
2012-02-10 10:15:01 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyMusketeer::Application.config.secret_token = '8d4912bfb78647d73eeaacef57f453adacb1044e36d0494e414f1aee2e4a13bc624bd4deaa45da6e63aeff6f3e2b046479a496e4aa24ca389ef23527cce343d7'
